a98b87b2b87ebe5c6c8cd64546ef28d8779aef50
moustapharamadan <47821743+moustapharamadan@users.noreply.github.com>
2020-08-01 21:25:22 +0300

    0094. Binary Tree Inorder Traversal

---
+++ C++/0094_Binary_Tree_Inorder_Traversal.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <stack>
+#include <vector>
+
+struct TreeNode {
+public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class TraversalGraph
+{
+public:
+    std::vector<int> inorderTraversal(TreeNode* root)
+    {
+        std::stack<TreeNode*> nodesStack;
+        std::vector<int> result;
+
+        while (!nodesStack.empty() || nullptr != root)
+        {
+            while (nullptr != root)
+            {
+                nodesStack.push(root);
+                root = root->left;
+            }
+            root = nodesStack.top();
+            nodesStack.pop();
+            result.push_back(root->val);
+            root = root->right;
+        }
+        return result;
+    }
+    std::vector<int> recursive(TreeNode* root)
+    {
+        std::vector<int> result;
+        recursiveInternal(root,result);
+        return result;
+    }
+private:
+    void recursiveInternal(TreeNode* root, std::vector<int>& result)
+    {
+        if (nullptr != root)
+        {
+            recursiveInternal(root->left,result);
+            result.push_back(root->val);
+            recursiveInternal(root->right, result);
+        }
+    }
+};
